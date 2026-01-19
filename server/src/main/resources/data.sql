061257d9bf13684718d076567e5266ab4fc3d1f1
donghee290 <eehd1127@naver.com>
2026-01-19 21:04:06 +0900

    sql 수정

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO music (music_id, file_path, title, bpm, artists, genre, duration)
-VALUES (1, 'Cash Out.mp3', 'Cash Out', 160, 'Schrandy', 'Techno', 142)
-ON CONFLICT (music_id) DO NOTHING;
+INSERT INTO music (file_path, title, bpm, artists, genre, duration)
+VALUES ('Cash Out.mp3', 'Cash Out', 160, 'Schrandy', 'Techno', 142)
+ON CONFLICT (file_path) DO NOTHING;
 
-INSERT INTO music (music_id, file_path, title, bpm, artists, genre, duration)
-VALUES (2, 'harinezumi.mp3', 'harinezumi', 94, 'waera', 'House', 117)
-ON CONFLICT (music_id) DO NOTHING;
+INSERT INTO music (file_path, title, bpm, artists, genre, duration)
+VALUES ('harinezumi.mp3', 'harinezumi', 94, 'waera', 'House', 117)
+ON CONFLICT (file_path) DO NOTHING;
